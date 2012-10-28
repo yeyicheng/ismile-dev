544360f4d666ce4f4c41cf21d7533b33990f4c54
Yicheng Ye <ye.yicheng123@gmail.com>
2012-10-28 06:40:32 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ismile::Application.config.secret_token = 'ec337f3861b2d6b21da399ed72831e522d646df1d0972ec4b8a3a4e5d42956f6c8e2361883e331963bba3cc9eb6ddd5476dc6e5cb77a1c5e4fbb4eb61f8087f2'
