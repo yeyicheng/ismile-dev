3fa21b09a256bad5f4fa2b3e1542d9eea873a635
Yicheng Ye <ye.yicheng123@gmail.com>
2012-12-29 02:41:37 -0500

    multi-lang

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 	include UsersHelper
+
+	before_filter :set_locale
+	
+	protected
+	def set_locale
+		I18n.locale = params[:locale] || I18n.default_locale
+	end
 end
