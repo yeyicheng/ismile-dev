eee5c3393375b78b345c54d4c3a4b39a98b254ae
YichengYe <yyc@ubuntu.(none)>
2013-01-28 04:08:56 -0800

    job validates

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ActiveRecord::Base
-  attr_accessible :category, :description, :title
+	attr_accessible :category, :description, :title
+	validates :title, :category, :description, :presence => true
 end
